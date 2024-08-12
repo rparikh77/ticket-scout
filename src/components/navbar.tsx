ef699613ec2f2ff60e832b3324d90ae4095dd363
Rohan Parikh <raparikh10@gmail.com>
2024-08-12 11:52:23 -0500

    navbar-01

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -2,10 +2,11 @@ import React from 'react';
 import Logo from '../assets/ticketscoutlogo.png';
 import '../styles/navbar.css';
 
+
 const Navbar: React.FC = () => {
   return (
     <div className='navbar-container'>
-        <img src={Logo} alt='logo' className='logo'></img>
+        <img src={Logo} alt='logo'></img>
     </div>
   )
 }
