c0cf7b05be64c730bdb38095b00d6a113c72bd3c
Rohan Parikh <raparikh10@gmail.com>
2024-08-15 12:35:06 -0500

    saagar-pull

--- src/pages/live.tsx
+++ src/pages/live.tsx
@@ -3,7 +3,9 @@ import React from 'react';
 const Live: React.FC = () => {
   return (
     <div className='live-container'>
-      
+      <div className='nba-card'>
+        <h1>NBA</h1>
+      </div>
     </div>
   )
 }
